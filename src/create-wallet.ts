d49638c97bb26f56c20d6c47cfbc9475e6ef2078
Jia Jian Goi <jiajian@levain.tech>
2023-09-11 14:02:42 +0800

    feat(examples): adding examples (#1)

---
+++ src/create-wallet.ts
@@ -0,0 +1,197 @@
+import express from "express";
+import { Wallet } from "ethers";
+import { encrypt } from "@soufflejs/crypto";
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  gql,
+} from "@apollo/client/core";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: process.env.LEVAIN_API_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = process.env.LEVAIN_API_ACCESS_TOKEN;
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+// Create keys on Levain
+async function createKey(input: any) {
+  const CREATE_KEY = gql`
+    mutation CreateKey($input: CreateKeyInput!) {
+      createKey(input: $input) {
+        keyId
+        name
+        publicKey
+      }
+    }
+  `;
+
+  const response = await client.mutate({
+    mutation: CREATE_KEY,
+    variables: { input },
+  });
+
+  return response.data.createKey;
+}
+
+// Create a wallet on Levain
+async function createWallet(input: any) {
+  const CREATE_WALLET = gql`
+    mutation CreateWallet($input: CreateWalletInput!) {
+      createWallet(input: $input) {
+        walletId
+        organizationNetworkId
+        organizationNetwork {
+          network {
+            protocolName
+            networkName
+          }
+        }
+        name
+        status
+        mainAddress
+      }
+    }
+  `;
+
+  const response = await client.mutate({
+    mutation: CREATE_WALLET,
+    variables: { input },
+  });
+
+  return response.data.createWallet;
+}
+
+// Query for organization network IDs
+async function organizationNetworks(input: any) {
+  const ORG_NETWORKS = gql`
+    query OrganizationNetworks($orgId: ID!) {
+      organization(orgId: $orgId) {
+        networks {
+          edges {
+            node {
+              organizationNetworkId
+              network {
+                networkId
+                identifier
+                protocolName
+                networkName
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const response = await client.query({
+    query: ORG_NETWORKS,
+    variables: { input },
+  });
+
+  return response.data.organization;
+}
+
+// Endpoint to create wallets programatically
+app.get("/create-wallet", async (req, res) => {
+  try {
+    // Create and encrypt `Main` key, though we recommend you to already have your offline generation of keys elsewhere
+    const password = "CakeCEC@1234";
+    const mainKeyPair = Wallet.createRandom();
+    const mainPrivateKeyEncrypted = await encrypt(
+      password,
+      mainKeyPair.privateKey
+    );
+
+    // Create `Backup` key, but we don't need the private key, so please keep it safely offline
+    const backupKeyPair = Wallet.createRandom();
+
+    // console.log(mainPrivateKeyEncrypted);
+    // console.log(mainKeyPair.publicKey, mainKeyPair.privateKey);
+    // console.log(backupKeyPair.publicKey, backupKeyPair.privateKey);
+
+    const orgNetworks = await organizationNetworks({
+      orgId: process.env.LEVAIN_ORG_ID as string,
+    });
+
+    console.log(orgNetworks);
+
+    // Submit both public keys to Levain
+    const key1 = await createKey({
+      orgId: process.env.LEVAIN_ORG_ID as string,
+      name: "Key 1 created via API",
+      type: "SCALAR_NEUTERED",
+      publicKey: mainKeyPair.publicKey,
+      retrieveIfExists: false,
+    });
+    const key2 = await createKey({
+      orgId: process.env.LEVAIN_ORG_ID as string,
+      name: "Key 2 created via API",
+      type: "SCALAR_NEUTERED",
+      publicKey: backupKeyPair.publicKey,
+      retrieveIfExists: false,
+    });
+
+    const keyForWalletPassword = await createKey({
+      orgId: process.env.LEVAIN_ORG_ID as string,
+      name: "API-created RSA keypair for wallet password",
+      type: "RSA",
+      retrieveIfExists: false,
+    });
+
+    // Create wallet
+    const wallet = await createWallet({
+      orgId: process.env.LEVAIN_ORG_ID as string,
+      organizationNetworkId: "b60e5c14-59ce-4cea-ad34-309de16c12c6",
+      description: "API-created Levain Wallet powered by SimpleMultiSig",
+      type: "EvmContractSimpleMultiSig", // Alternatively, EvmContractSafe
+      name: "API-created Levain Wallet 1",
+      mainKey: {
+        keyId: key1.keyId,
+        passwordRecoveryKeyId: keyForWalletPassword.keyId,
+        encryptedPrivateKey: mainPrivateKeyEncrypted, // This is the encrypted private key
+      },
+      backupKey: {
+        keyId: key2.keyId,
+      },
+    });
+
+    // console.log(wallet);
+
+    // HTTP response
+    res.status(200).json({
+      message: "Successfully created a wallet using your keys. Please go to https://app.levain.tech/ to see your wallet.",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error });
+  }
+});
+
+app.listen(port, () => {
+  console.log(
+    `Demo service using Levain GraphQL APIs is running at http://localhost:${port}`
+  );
+});
