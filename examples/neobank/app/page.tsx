fe559284fbed41740500c31135bb9a8632e0dc73
Fuxing Loh <git@fuxing.dev>
2023-10-04 12:29:00 +0800

    chore(workflows): ci.yml build, prettier, & eslint

--- examples/neobank/app/page.tsx
+++ examples/neobank/app/page.tsx
@@ -3,9 +3,7 @@ import { ReactElement } from 'react';
 export default function Home(): ReactElement {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2 className="text-2xl font-semibold">
-        NeoBank
-      </h2>
+      <h2 className="text-2xl font-semibold">NeoBank</h2>
     </main>
   );
 }
