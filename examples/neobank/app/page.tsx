7334bfcc4cd3781e67da78e4902bb72b6cc208ef
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2023-10-09 17:11:11 +0800

    feat(neobank): scaffold example UI (#33)

--- examples/neobank/app/page.tsx
+++ examples/neobank/app/page.tsx
@@ -1,9 +1,52 @@
 import { ReactElement } from 'react';
+import Link from 'next/link';
+import { StyledTable } from '@/components/StyledTable';
 
-export default function Home(): ReactElement {
+export default function WalletPage(): ReactElement {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2 className="text-2xl font-semibold">NeoBank</h2>
+    <main className="mx-auto my-6 w-full max-w-screen-xl px-6 lg:my-10 lg:px-10">
+      <div className="border-mono-50 bg-mono-900 border">
+        <div className="p-5">
+          <h2>Omnibus Wallets</h2>
+        </div>
+        <StyledTable>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Blockchain CAIP19 Identifier</th>
+              <th>Link to Scan</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Wallet #1</td>
+              <td>caip2:eip155:421613</td>
+              <td>0xd54c050540bb15c7be6e16d8f65dbd42a890414e2b6a0b96b7312a5bbcb8ae94</td>
+              <td>
+                <div className="flex">
+                  <Link href="" className="border-mono-50 block border px-4 py-1 font-bold">
+                    Send
+                  </Link>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>Wallet #1</td>
+              <td>caip2:eip155:421613</td>
+              <td>0xd54c050540bb15c7be6e16d8f65dbd42a890414e2b6a0b96b7312a5bbcb8ae94</td>
+              <td>
+                <div className="flex">
+                  <Link href="" className="border-mono-50 block border px-4 py-1 font-bold">
+                    Send
+                  </Link>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </StyledTable>
+        <div className="border-mono-50 border-t px-5 py-2.5 text-sm">Total Records:</div>
+      </div>
     </main>
   );
 }
