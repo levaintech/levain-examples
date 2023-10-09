0398ded05d7b9cca4e9fcc9b3dc88271b44af54e
Fuxing Loh <git@fuxing.dev>
2023-10-09 09:38:46 +0800

    fix lint and format

--- packages/create-levain-app/src/ProjectProgram.ts
+++ packages/create-levain-app/src/ProjectProgram.ts
@@ -5,11 +5,10 @@ import ExampleIndex from '@levain-examples/examples/dist/Example/index.json';
 import { green } from 'picocolors';
 import prompts from 'prompts';
 
-import { isFolderEmpty } from './utils/FolderEmpty';
-import { isNpmNameValid } from './utils/NpmNameValid';
-
 // @ts-ignore - this file will always be present
 import { version } from '../package.json';
+import { isFolderEmpty } from './utils/FolderEmpty';
+import { isNpmNameValid } from './utils/NpmNameValid';
 
 export interface ProjectConfig {
   path: string;
