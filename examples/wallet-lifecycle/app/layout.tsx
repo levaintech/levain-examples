00e5c3265f4cce9d4ffd8bba68879fc1f261c105
Eli <elicodesforfun@gmail.com>
2023-10-10 13:46:54 +0800

    sync

--- examples/wallet-lifecycle/app/layout.tsx
+++ examples/wallet-lifecycle/app/layout.tsx
@@ -1,13 +1,14 @@
 import './globals.css';
 import { ReactElement, ReactNode } from 'react';
 import { Metadata } from 'next';
-import { ThemeScript } from '@/components/ThemeSelector';
+import { ThemeScript, ThemeSelector } from '@/components/ThemeSelector';
 import { MadeWithLevain } from '@/components/MadeWithLevain';
-import { RootHeader } from '@/app/RootHeader';
 
 export const metadata: Metadata = {
   title: 'Wallet Lifecycle — Levain Examples',
-  description: 'Built with Levain, Next.js and Tailwind CSS.',
+  description:
+    'A demo app built with Levain, Next.js and Tailwind CSS. ' +
+    'Start, run and grow your crypto business with enterprise-grade security and self-custody wallet infrastructure.',
 };
 
 export default function RootLayout(props: { children: ReactNode }): ReactElement {
@@ -17,8 +18,15 @@ export default function RootLayout(props: { children: ReactNode }): ReactElement
         <ThemeScript />
       </head>
       <body className="bg-mono-950 text-mono-200">
-        <RootHeader />
+        <div className="mx-auto mt-6 w-full max-w-screen-xl px-6 lg:mt-10 lg:px-10">
+          <div className="flex justify-between">
+            <h1 className="text-mono-50 text-3xl font-bold">Levain NeoBank</h1>
+            <ThemeSelector />
+          </div>
+        </div>
+
         {props.children}
+
         <MadeWithLevain />
       </body>
     </html>
--- examples/wallet-lifecycle/app/transactions/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactElement } from 'react';
-
-export default function TransactionsPage(): ReactElement {
-  return <div></div>;
-}
