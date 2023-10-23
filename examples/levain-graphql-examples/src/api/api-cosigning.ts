a999fb701d0d986c7c7ad5e4ac100fa915f59d5f
Jia Jian Goi <jiajian@levain.tech>
2023-10-23 10:17:39 +0800

    feat(examples): executeTransaction and custom tx co-signing (#46)
    * feat(examples): update examples w executeTransaction
    
    * fix(lint): lint
    
    * fix(typos): explorer links and ts-ignore for fetch

--- examples/levain-graphql-examples/src/api/api-cosigning.ts
+++ examples/levain-graphql-examples/src/api/api-cosigning.ts
@@ -7,7 +7,7 @@ import {
   approveTransactionRequest,
   createTransactionDigests,
   createTransactionRequest,
-  signTransactionRequest,
+  executeTransaction,
 } from '../utils/mutations';
 
 dotenv.config();
@@ -28,14 +28,16 @@ router.get('/process-withdrawal', async (req, res) => {
      * Once all checks have passed, create a transaction request via Levain
      */
 
-    const provider = new ethers.JsonRpcProvider(process.env.PROVIDER_BASE_URL);
+    // This example requires an Ethereum Sepolia Testnet wallet, and its associated private key
+    const walletId = process.env.LEVAIN_OPS_WITHDRAWAL_HOT_WALLET_ID as string;
+    const encryptedPrivateKeyFile = 'api-cosigner-private-key-sepolia.json';
 
     const withdrawalAddress = req.query.address;
 
     // Create tx request via Levain from Ops Withdrawal Hot Wallet to the user's wallet
     const createTxRequest = await createTransactionRequest({
       orgId: process.env.LEVAIN_ORG_ID as string,
-      walletId: process.env.LEVAIN_OPS_WITHDRAWAL_HOT_WALLET_ID as string,
+      walletId,
       // networkAssetId: 'abbd6617-1783-444f-84f5-62514c54912c', // Ether on Ethereum Goerli Testnet
       networkAssetId: '4195f3f1-c04a-4fd3-8b86-27061502814d', // Ether on Ethereum Sepolia Testnet
       transactionData: {
@@ -62,7 +64,7 @@ router.get('/process-withdrawal', async (req, res) => {
     // Create transaction digests using the requestId
     const createTxDigests = await createTransactionDigests({
       orgId: process.env.LEVAIN_ORG_ID as string,
-      walletId: process.env.LEVAIN_OPS_WITHDRAWAL_HOT_WALLET_ID as string,
+      walletId,
       requestId: createTxRequest.requestId,
     });
 
@@ -70,7 +72,7 @@ router.get('/process-withdrawal', async (req, res) => {
     console.log('Digest to be API co-signed', digestToApiCosign);
 
     // This contains sensitive information: the encrypted private key. You will sign transactions using this key
-    const apiCoSignerPrivateKey = JSON.parse(fs.readFileSync('api-cosigner-private-key.json', 'utf-8'));
+    const apiCoSignerPrivateKey = JSON.parse(fs.readFileSync(encryptedPrivateKeyFile, 'utf-8'));
 
     const privateKey = decrypt(
       process.env.LEVAIN_USER_SIGNING_KEY_PASSWORD as string,
@@ -84,24 +86,19 @@ router.get('/process-withdrawal', async (req, res) => {
 
     console.log('signatureWithoutV', signatureWithoutV);
 
-    // Submit the signed transaction to Levain to co-sign to get the other signature
-    const signTxRequest = await signTransactionRequest({
+    // Submit the signed transaction to Levain to co-sign and execute for us, gas estimation is handled by Levain and funds are drawn from the gas tank
+    const executedTx = await executeTransaction({
       orgId: process.env.LEVAIN_ORG_ID as string,
-      walletId: process.env.LEVAIN_OPS_WITHDRAWAL_HOT_WALLET_ID as string,
+      walletId,
       requestId: createTxRequest.requestId,
       signature: signatureWithoutV,
     });
 
-    // The actual signed transaction that can be broadcasted on-chain, signed by gas tank
-    const signedTx = signTxRequest.transactionSigned;
-    const tx = await provider.send('eth_sendRawTransaction', [signedTx]);
-    console.log(`https://goerli.etherscan.io/tx/${tx}`);
-
     // HTTP response
     res.status(200).json({
       message: 'Successfully co-signed transaction using Levain GraphQL APIs',
-      tx: tx,
-      etherscanTx: `https://goerli.etherscan.io/tx/${tx}`,
+      tx: executedTx.transactionHash,
+      txExplorerLink: `https://sepolia.etherscan.io/tx/${executedTx.transactionHash}`,
     });
   } catch (error) {
     console.log(error);
