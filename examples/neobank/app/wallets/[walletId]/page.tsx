fb5d3b3b5a021ea6148ff07655facac4db0db379
Joel-David <git@joeldavid.dev>
2023-12-08 18:31:37 +0800

    Added wallet overview

--- examples/neobank/app/wallets/[walletId]/page.tsx
+++ examples/neobank/app/wallets/[walletId]/page.tsx
@@ -1,43 +1,79 @@
-'use client';
+import React, { ReactElement } from 'react';
+import { Jumbotron, JumbotronHeader, JumbotronRow } from '@/components/Jumbotron';
+import { createGraphClient } from '@/components/RequestGraph';
+import { FormGroupField } from '@/components/forms/FormGroup';
+import { Field } from 'formik';
+import clsx from 'clsx';
 
-import { ReactElement } from 'react';
-import { FormGroup, FormGroupButton, FormGroupField, FormGroupFieldSelect } from '@/components/forms/FormGroup';
-import { Jumbotron, JumbotronRow } from '@/components/Jumbotron';
+export default async function WalletOverviewPage(props: {
+  params: {
+    walletId: string;
+  };
+}): ReactElement {
+  const graphClient = createGraphClient();
 
-export default function WalletOverviewPage(): ReactElement {
+  const response: any = await graphClient.request(`
+    query WalletOverviewQuery {
+      wallet(walletId: "${props.params.walletId}") {
+        name
+        mainAddress
+        status
+        description
+        organizationNetwork{
+          network {
+            protocolName
+            identifier
+          }
+        }
+        depositAddresses {
+          totalCount
+        }
+      }
+    }
+`);
+
+  const wallet = response.wallet;
+
   return (
     <div className="mt-16 flex flex-wrap justify-center">
       <div className="w-2/3">
         <Jumbotron>
-          <FormGroup
-            validateOnBlur={false}
-            className="flex flex-col"
-            onSubmit={(form) => {}}
-            initialValues={{
-              amount: 0,
-            }}
-          >
-            <JumbotronRow>
-              <FormGroupField label="Amount" type="number" name="amount" value={0} />
-              <FormGroupField label="Asset" type="number" name="amount" value={0} />
-            </JumbotronRow>
-            <JumbotronRow>
-              <FormGroupFieldSelect className="col-span-2" label="From" type="number" name="amount" value={0} />
-            </JumbotronRow>
-            <JumbotronRow>
-              <FormGroupField
-                className="col-span-2"
-                label="To"
-                type="text"
-                name="to"
-                placeholder="Input address here"
-              />
-            </JumbotronRow>
-            <div className="m-5 mt-10 flex justify-end">
-              <FormGroupButton className="!bg-brand-500/90 !px-4 text-white transition">Send</FormGroupButton>
-            </div>
-          </FormGroup>
+          <JumbotronHeader title={`${wallet.name}`} />
+          <JumbotronRow>
+            <WalletDetail label="Wallet ID" value={props.params.walletId} />
+            <WalletDetail label="Status" value={wallet.status} />
+          </JumbotronRow>
+          <JumbotronRow />
+          <JumbotronRow>
+            <WalletDetail label="Address" value={wallet.mainAddress} />
+          </JumbotronRow>
+          <JumbotronRow>
+            <WalletDetail label="Network" value={wallet.organizationNetwork.network.protocolName} />
+            <WalletDetail label="CAIP-2 Identifier" value={wallet.organizationNetwork.network.identifier} />
+          </JumbotronRow>
+          <JumbotronRow />
+          <JumbotronRow>
+            <WalletDetail label="No. of Deposit Addresses" value={wallet.depositAddresses.totalCount} />
+          </JumbotronRow>
         </Jumbotron>
+      </div>
+    </div>
+  );
+}
+
+function WalletDetail(props: { label: string; value: string }): ReactElement {
+  return (
+    <div className="text-mono-400 block select-none text-sm">
+      {props.label}
+      <div
+        className={clsx(
+          'bg-invert/5 text-mono-50 mt-3 w-full rounded border-none px-4 py-3 text-sm',
+          'focus:ring-mono-50 focus:ring-1',
+          'aria-invalid:ring-1 aria-invalid:ring-red-500',
+          'disabled:text-mono-500',
+        )}
+      >
+        {props.value}
       </div>
     </div>
   );
