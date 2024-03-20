ed26909bd6f0d9742d2fc23011733c49aeb82a90
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2024-03-20 16:41:33 +0800

    chore(packages): flatten @stickyjs deps (#96)

---
+++ workspace/eslint-config/index.js
@@ -0,0 +1,42 @@
+const tseslint = require('typescript-eslint');
+const globals = require('globals');
+
+module.exports = tseslint.config(
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  {
+    ignores: ['**/*.js', '**/*.d.ts', '**/*.d.ts.map'],
+  },
+  require('@eslint/js').configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      'simple-import-sort': require('eslint-plugin-simple-import-sort'),
+      import: require('eslint-plugin-import'),
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+    },
+  },
+  {
+    plugins: {
+      'no-only-tests': require('eslint-plugin-no-only-tests'),
+    },
+    rules: {
+      'no-only-tests/no-only-tests': 'error',
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  require('eslint-config-prettier'),
+);
